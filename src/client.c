fccea32eed6b9428c553f77762f72370ee5c5dac
Aniol0012 <aniol0012@gmail.com>
2023-04-11 14:08:21 +0200

    v77

--- src/client.c
+++ src/client.c
@@ -14,7 +14,57 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <pthread.h>
-#include "estructures.h"
+
+/*Estructura per a guardar les dades del arxiu del client */
+struct client_config{
+  char name[7];
+  char MAC[13];
+  char server[20];
+  char random[7];
+  int UDPport;
+  int TCPport;
+};
+
+/*Estructura per a guardar dades per a comprovar als ALIVE */
+struct server_data{
+  char name[7];
+  char MAC[13];
+  char random[7];
+};
+
+/* Estructura per a pasar les structs de config a treatPacket() */
+struct parameters{
+  struct client_config *config;
+  struct sockaddr_in addr_cli;
+  struct sockaddr_in udp_addr_server;
+  struct sockaddr_in tcp_addr_server;
+  struct udp_PDU *data;
+};
+
+/*Estructura que fa de paquet UDP */
+struct udp_PDU{
+  unsigned char type;
+  char name[7];
+  char mac[13];
+  char random[7];
+  char data [50];
+};
+
+/*Estructura que fa de paquet TCP */
+struct tcp_PDU{
+  unsigned char type;
+  char name[7];
+  char mac[13];
+  char random[7];
+  char data [150];
+};
+
+/* Estructura que conté la info que es rep del subscribe per a la conexió TCP */
+struct tcp_data{
+  char random[8];
+  int tcp_port;
+};
+
 
 // TIPUS DE PAQUET FASE DE REGISTRE
 #define REGISTER_REQ 0x00  // Petició de resgistre
